1807db27fcfc60139c0eba4e630b076d836ef302
bbkaeul <86936658+bbkaeul@users.noreply.github.com>
2023-08-10 20:05:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,68 +1,281 @@
 import os
-import re
-import time
-import json
-import copy
-import types 
-import argparse
-import gradio as gr 
-from os import listdir
-from os.path import isfile, join
+from typing import Iterator, List, Tuple
+import torch
+import gradio as gr
+from distutils.util import strtobool
+import transformers.utils
+from configs import TrainArguments
+from transformers import AutoTokenizer, pipeline, logging, HfArgumentParser
+from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 
-from transformers import AutoModelForCausalLM
+parser = HfArgumentParser(TrainArguments)    
+args = parser.parse_args_into_dataclasses()[0]  
+
+# model = model(
+#     model_path=args.MODEL_PATH,
+#     backend_type=args.BACKEND_TYPE,
+#     max_tokens=args.MAX_INPUT_TOKEN_LENGTH,
+#     load_in_8bit=args.LOAD_IN_8BIT,
+#     # verbose=True,
+# )
+
+print("Step 0: Load model.")
+use_triton = False
+tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, use_fast=True)
+model = AutoGPTQForCausalLM.from_quantized(args.model_name_or_path,
+        model_basename=args.model_basename,
+        use_safetensors=True,
+        trust_remote_code=True,
+        device="cuda:0",
+        use_triton=use_triton,
+        quantize_config=None)
+
+def clear_and_save_textbox(message: str) -> tuple[str, str]:
+    return "", message
 
 
-
-# prompts by each model
-prompt_map = {
-    "llama2": ,
-    "stablebelguna": ,
-    "starcoder": ,
-    "gpt4": ,
-}
-
-# response configs by each model 
-response_configs = [
-    f"configs/response_configs/{model_config}" for model_config in listdir("configs/response_configs") if isfile(join("configs/response_configs", model_config))
-]
+def display_input(
+    message: str, history: list[tuple[str, str]]
+) -> list[tuple[str, str]]:
+    history.append((message, ""))
+    return history
 
 
-def move_to_task_select_view():
-    return (
-        "move to task select view",
-        gr.update(visible=False),
-        gr.update(visible=True),
+def delete_prev_fn(history: list[tuple[str, str]]) -> tuple[list[tuple[str, str]], str]:
+    try:
+        message, _ = history.pop()
+    except IndexError:
+        message = ""
+    return history, message or ""
+
+
+def generate(
+    message: str,
+    history_with_input: list[tuple[str, str]],
+    system_prompt: str,
+    max_new_tokens: int,
+    temperature: float,
+    top_p: float,
+    top_k: int,
+) -> Iterator[list[tuple[str, str]]]:
+    if max_new_tokens > args.MAX_MAX_NEW_TOKENS:
+        raise ValueError
+
+    history = history_with_input[:-1]
+    generator = model.run(
+        message, history, system_prompt, max_new_tokens, temperature, top_p, top_k
+    )
+    try:
+        first_response = next(generator)
+        yield history + [(message, first_response)]
+    except StopIteration:
+        yield history + [(message, "")]
+    for response in generator:
+        yield history + [(message, response)]
+
+
+def process_example(message: str) -> tuple[str, list[tuple[str, str]]]:
+    generator = generate(message, [], args.DEFAULT_SYSTEM_PROMPT, 1024, 1, 0.95, 50)
+    for x in generator:
+        pass
+    return "", x
+
+
+def check_input_token_length(
+    message: str, chat_history: list[tuple[str, str]], system_prompt: str
+) -> None:
+    input_token_length = model.get_input_token_length(
+        message, chat_history, system_prompt
+    )
+    if input_token_length > args.MAX_INPUT_TOKEN_LENGTH:
+        raise gr.Error(
+            f"The accumulated input is too long ({input_token_length} > {args.MAX_INPUT_TOKEN_LENGTH}). Clear your chat history and try again."
+        )
+
+
+with gr.Blocks(css="style.css") as demo:
+    print("Step 1: Gradio start.")
+    gr.Markdown(args.DESCRIPTION)
+
+    with gr.Group():
+        chatbot = gr.Chatbot(label="Chatbot")
+        with gr.Row():
+            textbox = gr.Textbox(
+                container=False,
+                show_label=False,
+                placeholder="Type a message.",
+                scale=10,
+            )
+            submit_button = gr.Button("Submit", variant="primary", scale=1, min_width=0)
+    with gr.Row():
+        retry_button = gr.Button("🔄  Retry", variant="secondary")
+        undo_button = gr.Button("↩️ Undo", variant="secondary")
+        clear_button = gr.Button("🗑️  Clear", variant="secondary")
+
+    saved_input = gr.State()
+
+    with gr.Accordion(label="Advanced options", open=False):
+        system_prompt = gr.Textbox(
+            label="System prompt", value=args.DEFAULT_SYSTEM_PROMPT, lines=6
+        )
+        max_new_tokens = gr.Slider(
+            label="Max new tokens",
+            minimum=1,
+            maximum=args.MAX_MAX_NEW_TOKENS,
+            step=1,
+            value=args.DEFAULT_MAX_NEW_TOKENS,
+        )
+        temperature = gr.Slider(
+            label="Temperature",
+            minimum=0.1,
+            maximum=4.0,
+            step=0.1,
+            value=1.0,
+        )
+        top_p = gr.Slider(
+            label="Top-p (nucleus sampling)",
+            minimum=0.05,
+            maximum=1.0,
+            step=0.05,
+            value=0.95,
+        )
+        top_k = gr.Slider(
+            label="Top-k",
+            minimum=1,
+            maximum=1000,
+            step=1,
+            value=50,
+        )
+
+    gr.Examples(
+        examples=[
+            "Hi! How are you doing?",
+            "Can you explain briefly to me what is the Python programming language?",
+            "Explain the plot of Cinderella in a sentence.",
+            "How many hours does it take a man to eat a Helicopter?",
+            "Write a 100-word article on 'Benefits of Open-Source in AI research'",
+        ],
+        inputs=textbox,
+        outputs=[textbox, chatbot],
+        fn=process_example,
+        cache_examples=True,
     )
 
-def use_chosen_task():
-    try:
-        # test = global_vars.model
-    except AttributeError:
-        raise gr.Error("There is no chosen task previously.")
+    textbox.submit(
+        fn=clear_and_save_textbox,
+        inputs=textbox,
+        outputs=[textbox, saved_input],
+        api_name=False,
+        queue=False,
+    ).then(
+        fn=display_input,
+        inputs=[saved_input, chatbot],
+        outputs=chatbot,
+        api_name=False,
+        queue=False,
+    ).then(
+        fn=check_input_token_length,
+        inputs=[saved_input, chatbot, system_prompt],
+        api_name=False,
+        queue=False,
+    ).success(
+        fn=generate,
+        inputs=[
+            saved_input,
+            chatbot,
+            system_prompt,
+            max_new_tokens,
+            temperature,
+            top_p,
+            top_k,
+        ],
+        outputs=chatbot,
+        api_name=False,
+    )
 
-    gen_config = 
+    button_event_preprocess = (
+        submit_button.click(
+            fn=clear_and_save_textbox,
+            inputs=textbox,
+            outputs=[textbox, saved_input],
+            api_name=False,
+            queue=False,
+        )
+        .then(
+            fn=display_input,
+            inputs=[saved_input, chatbot],
+            outputs=chatbot,
+            api_name=False,
+            queue=False,
+        )
+        .then(
+            fn=check_input_token_length,
+            inputs=[saved_input, chatbot, system_prompt],
+            api_name=False,
+            queue=False,
+        )
+        .success(
+            fn=generate,
+            inputs=[
+                saved_input,
+                chatbot,
+                system_prompt,
+                max_new_tokens,
+                temperature,
+                top_p,
+                top_k,
+            ],
+            outputs=chatbot,
+            api_name=False,
+        )
+    )
 
-# gradio 
-def flip_text(x):
-    return x[::-1]
-def flip_code(x):
-    return x[::-1]
+    retry_button.click(
+        fn=delete_prev_fn,
+        inputs=chatbot,
+        outputs=[chatbot, saved_input],
+        api_name=False,
+        queue=False,
+    ).then(
+        fn=display_input,
+        inputs=[saved_input, chatbot],
+        outputs=chatbot,
+        api_name=False,
+        queue=False,
+    ).then(
+        fn=generate,
+        inputs=[
+            saved_input,
+            chatbot,
+            system_prompt,
+            max_new_tokens,
+            temperature,
+            top_p,
+            top_k,
+        ],
+        outputs=chatbot,
+        api_name=False,
+    )
 
-with gr.Blocks() as demo:
-    gr.Markdown("Text-based generation or Code-based generation using this demo.")
-    with gr.Tab("Text-based generation"):
-        text_input =gr.Textbox(label="message")
-        text_output =gr.Textbox(label="output") # 변경
-        parameter_box = gr.CheckboxGroup(["", "", ""])
-        text_button = gr.Button("send")
-    with gr.Tab("Code-based generation"):
-        code_input =gr.Textbox(label="message")
-        code_output =gr.Textbox(label="output") #  변경
-        code_button = gr.Button("send")
-    with gr.Accordion(""):
-        gr.Markdown("")
-    text_button.click(flip_text, inputs=text_input, outputs=text_output)
-    code_button.click(flip_code, inputs=code_input, outputs=code_output)
+    undo_button.click(
+        fn=delete_prev_fn,
+        inputs=chatbot,
+        outputs=[chatbot, saved_input],
+        api_name=False,
+        queue=False,
+    ).then(
+        fn=lambda x: x,
+        inputs=[saved_input],
+        outputs=textbox,
+        api_name=False,
+        queue=False,
+    )
 
-demo.launch()
-# https://www.gradio.app/guides/controlling-layout
+    clear_button.click(
+        fn=lambda: ([], ""),
+        outputs=[chatbot, saved_input],
+        queue=False,
+        api_name=False,
+    )
+
+demo.queue(max_size=20).launch()
---
+++ configs.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+@dataclass
+class TrainArguments(object):
+    model_name_or_path: Optional[str] = field(
+        default="TheBloke/Llama-2-7b-Chat-GPTQ",
+        metadata={"help": "model_name_or_path"},
+    )
+    model_basename: Optional[str] = field(
+        default="gptq_model-4bit-128g",
+        metadata={"help": "model_basename"},
+    )
+    DEFAULT_SYSTEM_PROMPT: Optional[str] = field(
+        default="",
+        metadata={"help": "DEFAULT_SYSTEM_PROMPT"},
+    )
+    MAX_MAX_NEW_TOKENS: Optional[int] = field(
+        default=2048,
+        metadata={"help": "MAX_MAX_NEW_TOKENS"},
+    )
+    DEFAULT_MAX_NEW_TOKENS: Optional[int] = field(
+        default=1024,
+        metadata={"help": "MAX_MAX_NEW_TOKENS"},
+    )
+    MAX_INPUT_TOKEN_LENGTH: Optional[int] = field(
+        default=4000,
+        metadata={"help": "MAX_MAX_NEW_TOKENS"},
+    )
+    MODEL_PATH: Optional[str] = field(
+        default="TheBloke/Llama-2-7b-Chat-GPTQ",
+        metadata={"help": "MODEL_PATH"},
+    )
+    BACKEND_TYPE: Optional[str] = field(
+        default=".env.7b_gptq_example",
+        metadata={"help": "BACKEND_TYPE"},
+    )
+    LOAD_IN_8BIT: Optional[bool] = field(
+        default=True,
+        metadata={"help": "LOAD_IN_8BIT"},
+    )
+    LOAD_IN_4BIT: Optional[bool] = field(
+        default=False,
+        metadata={"help": "LOAD_IN_4BIT"},
+    )
+    DESCRIPTION: Optional[str] = field(
+        default="""
+# llama2-webui
+This is a chatbot based on Llama-2. 
+- Supporting models: 
+    [Llama-2-7b](https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML)
+    [13b](https://huggingface.co/llamaste/Llama-2-13b-chat-hf)
+    [70b](https://huggingface.co/llamaste/Llama-2-70b-chat-hf)
+    all [Llama-2-GPTQ](https://huggingface.co/TheBloke/Llama-2-7b-Chat-GPTQ)
+    all [Llama-2-GGML](https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML)
+
+- Supporting model backends: 
+    [tranformers](https://github.com/huggingface/transformers)
+    [bitsandbytes(8-bit inference)](https://github.com/TimDettmers/bitsandbytes)
+    [AutoGPTQ(4-bit inference)](https://github.com/PanQiWei/AutoGPTQ)
+    [llama.cpp](https://github.com/ggerganov/llama.cpp)
+""",
+        metadata={"help": "DESCRIPTION"},
+    )
